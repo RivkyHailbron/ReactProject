d6213235924ef8e23bd7335baf40a6b8e455edab
RivkyHailbron <rivky2712@gmail.com>
2025-03-24 20:30:47 +0200

    New commit message

--- src/components/ProducerDetails.tsx
+++ src/components/ProducerDetails.tsx
@@ -1,4 +1,4 @@
 
 export const ProducerDetails = () => {
-    
+    return <></>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import { MenuProducer } from './components/MenuProducer.tsx'
 import { EventListUser } from './components/EventListUser.tsx'
 import { EventDetailsUser } from './components/EventDetailsUser.tsx'
 import { EventsProvider } from './context/Events.context.tsx'
+import { AddProducer } from './components/AddProducer.tsx'
+import { ProducerDetails } from './components/ProducerDetails.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,9 +19,10 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path='/' element={<App />}>
             <Route path='producer' element={<MenuProducer />}></Route>
-            <Route path='user' element={< EventListUser />}>
-              <Route path=':id' element={<EventDetailsUser />}></Route>
-            </Route>
+            <Route path='user' element={< EventListUser />}></Route>
+            <Route path="/user/:id" element={<EventDetailsUser />} />
+            <Route path="/producers/AddProducer" element={<AddProducer />} />
+            <Route path="/producers/ProducerDetails/:email" element={<ProducerDetails />} />
           </Route>
         </Routes>
       </EventsProvider>
